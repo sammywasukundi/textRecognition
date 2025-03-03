6bc3eaca7b0f90962ac32d2db2be0e5f9faa0695
sammyzasukundi <brosammy1et2@gmail.com>
2025-03-04 00:36:45 +0200

    add of filters button

--- lib/views/enhance_view.dart
+++ lib/views/enhance_view.dart
@@ -5,6 +5,8 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image/image.dart' as img;
+import 'package:image_editor_plus/image_editor_plus.dart';
+//import 'package:image_gallery_saver/image_gallery_saver.dart';
 
 import 'theme/app_colors.dart';
 
@@ -43,19 +45,42 @@ class _RecognizeViewState extends State<EnhanceView> {
         appBar: AppBar(
           iconTheme: IconThemeData(color: whiteColor),
           actions: [
-            IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: Icon(
-                Icons.change_circle_rounded,
-                color: whiteColor,
+            InkWell(
+              // onTap: () async {
+              //   final result = await ImageGallerySaver.saveImage(
+              //       Uint8List.fromList(img.encodePng(inputImage)));
+              //   print(result);
+              // },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(Icons.save_alt, color: whiteColor),
               ),
-            )
+            ),
+            InkWell(
+              onTap: () async {
+                final editedImage = await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ImageFilters(
+                      image: Uint8List.fromList(img.encodePng(inputImage)), // <-- Uint8List of image
+                    ),
+                  ),
+                );
+                inputImage = img.decodeImage(editedImage)!;
+                setState(() {
+                  inputImage;
+                });
+                enhanceImage();
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(Icons.filter, color: whiteColor),
+              ),
+            ),
           ],
           centerTitle: true,
           backgroundColor: blueColor,
-          title: Text('Agrandir', style: TextStyle(color: whiteColor)),
+          title: Text('Améliorer', style: TextStyle(color: whiteColor)),
         ),
         body: SingleChildScrollView(
           child: Container(
@@ -63,37 +88,53 @@ class _RecognizeViewState extends State<EnhanceView> {
               children: [
                 Image.memory(Uint8List.fromList(img.encodeBmp(inputImage))),
                 Container(
-                  margin: EdgeInsets.only(left: 15,right: 15),
+                  margin: EdgeInsets.only(left: 15, right: 15),
                   child: Row(
                     children: [
                       Icon(Icons.contrast, color: blueColor),
                       Expanded(
-                        child: Slider(value: contrast, onChanged: (v) {
-                          contrast = v;
-                          enhanceImage();
-                          setState(() {
-                            contrast;
-                          });
-                        },min: 80,max: 200,divisions: 12,
-                                label: contrast.toStringAsFixed(2),activeColor: blueColor,),
+                        child: Slider(
+                          value: contrast,
+                          onChanged: (v) {
+                            contrast = v;
+                            enhanceImage();
+                            setState(() {
+                              contrast;
+                            });
+                          },
+                          min: 80,
+                          max: 200,
+                          divisions: 12,
+                          label: contrast.toStringAsFixed(2),
+                          activeColor: blueColor,
+                        ),
                       ),
                     ],
                   ),
                 ),
                 Container(
-                  margin: EdgeInsets.only(left: 15,right: 15),
+                  margin: EdgeInsets.only(left: 15, right: 15),
                   child: Row(
                     children: [
-                      Icon(Icons.brightness_1,color: blueColor,),
+                      Icon(
+                        Icons.brightness_1,
+                        color: blueColor,
+                      ),
                       Expanded(
-                        child: Slider(value: brightness, onChanged: (v) {
-                          brightness = v;
-                          enhanceImage();
-                          setState(() {
-                            brightness;
-                          });
-                        },min: 1,max: 10,divisions: 10,
-                                label: brightness.toStringAsFixed(2),activeColor: blueColor),
+                        child: Slider(
+                            value: brightness,
+                            onChanged: (v) {
+                              brightness = v;
+                              enhanceImage();
+                              setState(() {
+                                brightness;
+                              });
+                            },
+                            min: 1,
+                            max: 10,
+                            divisions: 10,
+                            label: brightness.toStringAsFixed(2),
+                            activeColor: blueColor),
                       ),
                     ],
                   ),
